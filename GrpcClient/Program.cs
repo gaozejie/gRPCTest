79eb62f5839030154984d64dcfa1e850b7679c50
gaozejie <gaozejie@outlook.com>
2022-02-09 20:19:49 +0800

    submit

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+using GrpcService;
+using Newtonsoft.Json;
+
+Console.WriteLine("Hello, World!");
+
+//var channel = GrpcChannel.ForAddress("https://localhost:7145"); // 服务端地址
+//var client = new Greeter.GreeterClient(channel);
+//var reply = await client.SayHelloAsync(new HelloRequest { Name = "张三" });
+//Console.WriteLine("Greeter 服务返回数据: " + reply.Message);
+//Console.ReadKey();
+
+
+var channel = GrpcChannel.ForAddress("https://localhost:7145"); // 服务端地址
+var client = new User.UserClient(channel);
+var reply = await client.RegisterAsync(new UserDTO { Account = "zhangsan", Pwd = "123", Name = "张三", Age = 18 });
+Console.WriteLine("User 服务 RegisterAsync 方法返回数据: " + JsonConvert.SerializeObject(reply));
+Console.ReadKey();
+
+var reply1 = await client.RegisterAsync(new UserDTO { Account = "lisi", Pwd = "123", Name = "李四", Age = 20 });
+Console.WriteLine("User 服务 RegisterAsync 返回数据: " + JsonConvert.SerializeObject(reply1));
+Console.ReadKey();
+
+var reply2 = await client.GetAllUserAsync(new Google.Protobuf.WellKnownTypes.Empty());
+Console.WriteLine("User 服务 GetAllUserAsync 返回数据: " + JsonConvert.SerializeObject(reply2));
+Console.ReadKey();
+
+var reply3 = await client.LoginAsync(new LoginDTO() { Accont = "zhangsan", Pwd = "123" });
+Console.WriteLine("User 服务 LoginAsync 返回数据: " + JsonConvert.SerializeObject(reply3));
+Console.ReadKey();
+
+var reply4 = await client.LogoutAsync(new LogoutDTO() { Token = "TestToken" });
+Console.WriteLine("User 服务 LogoutAsync 返回数据: " + JsonConvert.SerializeObject(reply4));
+Console.ReadKey();
---
+++ GrpcService/Services/UserService.cs
@@ -0,0 +1,80 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+
+namespace GrpcService.Services
+{
+    public class UserService : User.UserBase
+    {
+        private readonly ILogger<UserService> _logger;
+        public UserService(ILogger<UserService> logger)
+        {
+            _logger = logger;
+        }
+
+        // 存储注册用户
+        private static IList<UserDTO> _userDTOs = new List<UserDTO>();
+
+        /// <summary>
+        /// 注册方法实现
+        /// </summary>
+        /// <param name="userDTO"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override Task<CommonResult> Register(UserDTO userDTO, ServerCallContext context)
+        {
+            _userDTOs.Add(userDTO);
+            return Task.FromResult(new CommonResult
+            {
+                Code = "2000",
+                Message = "注册成功。"
+            });
+        }
+
+        /// <summary>
+        /// 登陆方法实现
+        /// </summary>
+        /// <param name="userDTO"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override Task<LoginResult> Login(LoginDTO userDTO, ServerCallContext context)
+        {
+            // TODO...
+
+            return Task.FromResult(new LoginResult
+            {
+                Token = "TestToken"
+            });
+        }
+
+        /// <summary>
+        /// 退出方法实现
+        /// </summary>
+        /// <param name="userDTO"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override Task<CommonResult> Logout(LogoutDTO userDTO, ServerCallContext context)
+        {
+            // TODO...
+
+            return Task.FromResult(new CommonResult
+            {
+                Code = "2000",
+                Message = "退出成功。"
+            });
+        }
+
+        /// <summary>
+        /// 获取所有用户实现
+        /// </summary>
+        /// <param name="empty"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override Task<AllUser> GetAllUser(Empty empty, ServerCallContext context)
+        {
+            var allUser = new AllUser();
+            allUser.UserList.Add(_userDTOs);
+
+            return Task.FromResult(allUser);
+        }
+    }
+}
